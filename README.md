a631b4546c0e05eea4507539c4f15b3cffc4e5e4
OmkarArote <omkararote0@gmail.com>
2022-08-12 12:41:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-xycyw2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-xycyw2)
---
+++ vid1.js
@@ -0,0 +1,10 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+function add3no(a, b, c) {
+  return a + b + c;
+}
+
+d = add3no(5, 6, 9);
+console.log('we added 3 no.: ' + d);
