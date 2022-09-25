3c192b69a78fee3f0b19184d524dd1dc84a45b48
OmkarArote <64545577+OmkarArote@users.noreply.github.com>
2022-09-26 01:12:27 +0530

    1 file added

---
+++ vids/vid7.js
@@ -0,0 +1,13 @@
+//let a = prompt("Enter a number: ")
+// a = Number.parseInt(a)
+let a = 12;
+let b = 17;
+if (a > 0) {
+  console.log('Number is positive');
+} else if (a < 0) {
+  console.log('Number is negative');
+} else {
+  console.log('Number is a zero or not a number at all');
+}
+// Ternary Operator
+console.log('You can', b < 18 ? 'not vote' : 'vote');
